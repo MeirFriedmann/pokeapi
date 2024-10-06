42d79cdbdab186bae6eb8a6eba607a763ae642a7
Meir <friedmannmeir@gmail.com>
2024-10-06 10:03:51 +0300

    first commit

---
+++ pokeapi.py
@@ -0,0 +1,43 @@
+#ability, move, back_default
+import requests, json, random
+
+def get_pokemon() -> json:
+    try:
+        with open("pokemon.txt", 'r') as file:
+            pokemon_ids = {line.strip() for line in file}
+            file.close()
+    except FileNotFoundError:
+        pokemon_ids = set()
+    while True:
+        pokemon_id = random.randint(1, 1025)
+        if pokemon_id not in pokemon_ids:
+            with open("pokemon.txt", 'a') as file:
+                file.write(str(pokemon_id) + "\n")
+                file.close()
+            break
+    request = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+    pokemon = json.loads(request.content)
+    return pokemon
+
+
+
+def main():
+
+    while True:
+        asnwer = input("Do you want to draw a pokemon? (y/n)\n")
+        if asnwer == "yes" or asnwer == "y":
+            pokemon_json = get_pokemon()
+            print(f"Detailes for {pokemon_json["name"]}:")
+            print(f"Ability: {pokemon_json["abilities"][0]["ability"]["name"]}")
+            print(f"Move: {pokemon_json['moves'][0]['move']['name']}")
+            if pokemon_json['sprites']['back_default'] is None:
+                print("No picture of back was found.")
+            else:
+                print(f"Picture of back: {pokemon_json['sprites']['back_default']}")
+        elif asnwer == "no" or asnwer == "n":
+            exit("Farewell!")
+        else:
+            print("I didn't get that, try again")
+
+if __name__ == "__main__":
+    main()
